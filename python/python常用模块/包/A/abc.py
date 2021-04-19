b55f09283eb116da03d9bffcd2d6cc3b56da7544
mmarinroad <hghai@126.com>
2021-04-19 11:45:05 +0800

    1

--- python/python常用模块/包/A/abc.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-
-import apython  # 从当前目录引入 apython.py
-from ..B import he     # 从当前目录引入与之平行的目录下的 he.py
-
-test = 'Test' + apython.test() + '-' + he.test
---
+++ python/python常用模块/包（必须有__init__.py）/test.py
@@ -0,0 +1,11 @@
+
+import sys,os
+# sys.path.append(os.path.dirname(__file__) + os.sep + '../') 
+
+print(sys.path)
+
+from A import a1,a2
+from B import b1     # 从当前目录引入与之平行的目录下的 he.py
+
+test = a1.test + a2.test() + b1.test
+print(test)
