5a7080485137fb0e86635c3441e88e4555ccbb35
mmarinroad <hghai@126.com>
2021-04-26 12:27:00 +0800

    1

--- Go/语法基础/HelloWorld.go
+++ Go/语法基础/HelloWorld.go
@@ -1,11 +1,19 @@
-package main  	// 包名，如果是主函数所在问津，包名必须是main
+package main // 包名，如果是主函数所在问津，包名必须是main
 
 import (
-	"fmt" 		// import 导入包名
+	"fmt" // import 导入包名
 )
 
-// main 程序入口
-func main()  {
-	fmt.Println("Hello World")  // 包名.函数名()
-}
+/**
+ * @Description: main 函数入口
+ */
+func main() {
+	fmt.Println("Hello World") // 包名.函数名()
 
+	var x1 uint
+	var x, y, z = 1, 23, "hello"
+	u, t := 456, "World"
+	d := 789
+	h := 100
+	fmt.Println(x1, x, y, z, u, t, d, h)
+}
